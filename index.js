3ce1e7c1711a039391dc937c80345378fa791d00
TUA0910 <76881125+TUA0910@users.noreply.github.com>
2021-01-02 19:16:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = 'Nzk0OTUxOTI0NDc3OTg0Nzk5.X_CSrA.lQxlpW34cTmKpXZAaOHJoNmbGMc';
+const prefix = 'link!';
+
+bot.on('ready', () => {
+    console.log('You bot is now online.')
+    bot.user.setActivity('Frederiks hygge server', { type: "LISTENING"})
+        .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+        .catch(console.error);
+    bot.user.setStatus('dnd')
+        .then(console.log)
+        .catch(console.error);
+})
+
+bot.on("message", message => {
+    if(message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLocaleLowerCase();
+
+    if(command === 'invite') {
+        message.channel.send('**Invite folk til serveren med at skrive:**  https://discord.gg/6jgVe5g')
+    } else
+    if(command === 'website') {
+        message.channel.send('https://www.frederiks-hygge-server.net/')
+    } else
+    if(command=== 'commands') {
+        message.channel.send('`link!invite`')
+    } else (
+        message.channel.send('`link!website`')
+    )
+})
+
+bot.login(token);
